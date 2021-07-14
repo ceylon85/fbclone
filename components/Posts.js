80147b44426fca38b7f209b004194d4c63b517d4
ceylon85 <canleez123@gmail.com>
2021-07-14 18:15:48 +0900

    feed-postImage

--- components/Posts.js
+++ components/Posts.js
@@ -1,9 +1,27 @@
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
+
 function Posts() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [realtimePosts, loading, error] = useCollection(
+    db.collection("posts").orderBy("timestamp", "desc")
+  );
+
+  return (
+    <div>
+      {realtimePosts?.docs.map((post) => (
+        <Post
+          key={post.id}
+          name={post.data().name}
+          message={post.data().message}
+          email={post.data().email}
+          postImage={post.data().postImage}
+          image={post.data().image}
+          timestamp={post.data().timestamp}
+        />
+      ))}{" "}
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
