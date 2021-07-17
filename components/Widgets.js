1380697d61672eacf6698cfd34b855316f67012b
ceylon85 <canleez123@gmail.com>
2021-07-17 23:35:26 +0900

    widgets

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,22 +1,29 @@
-import {SearchIcon} from '@heroicons/react/outline';
-import {DotsHorizontalIcon, VideoCameraIcon} from '@heroicons/react/solid'
+import { SearchIcon } from "@heroicons/react/outline";
+import { DotsHorizontalIcon, VideoCameraIcon } from "@heroicons/react/solid";
 
 const contacts = [
-    { src: "https://links.papareact.com/f0p", name: "Jeff Bezoz" },
-    { src: "https://links.papareact.com/kxk", name: "Elon Musk" },
-    { src: "https://links.papareact.com/zvy", name: "Bill Gates" },
-    { src: "https://links.papareact.com/snf", name: "Mark Zuckerberg" },
-    { src: "https://links.papareact.com/d0c", name: "Harry Potter" },
-    { src: "https://links.papareact.com/6gg", name: "The Queen" },
-    { src: "https://links.papareact.com/r57", name: "James Bond" },
-]
+  { src: "https://links.papareact.com/f0p", name: "Jeff Bezoz" },
+  { src: "https://links.papareact.com/kxk", name: "Elon Musk" },
+  { src: "https://links.papareact.com/zvy", name: "Bill Gates" },
+  { src: "https://links.papareact.com/snf", name: "Mark Zuckerberg" },
+  { src: "https://links.papareact.com/d0c", name: "Harry Potter" },
+  { src: "https://links.papareact.com/6gg", name: "The Queen" },
+  { src: "https://links.papareact.com/r57", name: "James Bond" },
+];
 
 function Widgets() {
-    return (
-        <div>
-            
+  return (
+    <div className="flex-col hidden p-2 mt-5 lg:flex w-60">
+      <div className="flex items-center justify-between mb-5 text-gray-500">
+        <h2 className="text-xl">Contacts</h2>
+        <div className="flex space-x-2">
+          <VideoCameraIcon className="h-6" />
+          <SearchIcon className="h-6" />
+          <DotsHorizontalIcon className="h-6" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
